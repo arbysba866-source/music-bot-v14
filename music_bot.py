a86f2ad825e68b3492672c76ee0494187ca283d0
oussa <oussama.masajin@gmail.com>
2026-05-13 04:50:30 +0100

    fix user agent

--- music_bot.py
+++ music_bot.py
@@ -46,7 +46,7 @@ ytdl_format_options = {
     'ignoreerrors': False,
     'logtostderr': False,
     'quiet': True,
-    'no_warnings': True,
+    'http_headers': {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'},
     'default_search': 'auto',
     'source_address': '0.0.0.0'
 }
